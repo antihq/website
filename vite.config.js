1edcb6243efaef668cf7106d1d374c3d506309da
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2025-06-07 08:43:04 -0600

    wip

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
+import jigsaw from '@tighten/jigsaw-vite-plugin';
 import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+        jigsaw({
+            input: ['source/_assets/js/main.js', 'source/_assets/css/main.css'],
             refresh: true,
         }),
-        tailwindcss(),
     ],
 });
