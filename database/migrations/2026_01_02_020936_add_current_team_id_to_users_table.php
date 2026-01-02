f5dfe6c58aaec5b41bf42281dc581b2f0aa8862e
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2026-01-01 20:17:11 -0600

    db: remove foreign key constraint from current_team_id

--- database/migrations/2026_01_02_020936_add_current_team_id_to_users_table.php
+++ database/migrations/2026_01_02_020936_add_current_team_id_to_users_table.php
@@ -9,7 +9,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->foreignId('current_team_id')->nullable()->constrained('teams')->nullOnDelete();
+            $table->foreignId('current_team_id')->nullable();
         });
     }
 
