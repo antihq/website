bf5cc869d841ca2f0689a7a9edfd76d2d39c6fac
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2026-01-14 12:07:10 -0600

    ui: fix broken project links on welcome page so users can access the correct project URLs instead of being directed to dead or incorrect repositories

--- resources/views/pages/⚡welcome.blade.php
+++ resources/views/pages/⚡welcome.blade.php
@@ -21,7 +21,7 @@ new #[Layout('layouts::site')] class extends Component {
         <div class="flex flex-col gap-2">
             <flux:heading level="2" size="lg">antihq/board</flux:heading>
             <flux:text>
-                <flux:link href="https://github.com/antihq/password" target="_blank" rel="noopener noreferrer">
+                <flux:link href="https://github.com/antihq/board" target="_blank" rel="noopener noreferrer">
                     A collaborative project management tool with Kanban boards
                 </flux:link>.
             </flux:text>
@@ -30,7 +30,7 @@ new #[Layout('layouts::site')] class extends Component {
         <div class="flex flex-col gap-2">
             <flux:heading level="2" size="lg">antihq/password</flux:heading>
             <flux:text>
-                <flux:link href="https://github.com/antihq/password" target="_blank" rel="noopener noreferrer">
+                <flux:link href="https://password.antihq.com" target="_blank" rel="noopener noreferrer">
                     A secure password and credit card management application with team collaboration
                 </flux:link>.
             </flux:text>
