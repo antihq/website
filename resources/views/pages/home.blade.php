d4c9349cb311f31fba11f455e78edd50cdb98083
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2026-06-14 09:05:07 -0600

    move vault as first project item

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,10 +4,10 @@
     <h2 class="mt-6 font-bold">projects</h2>
 
     <ul class="lowercase ml-4">
+        <li><flux:link href="{{ route('vault') }}">Vault</flux:link></li>
         <li><flux:link href="https://2026-05-18-mayfly.antihq.com/" target="_blank">Mayfly</flux:link></li>
         <li><flux:link href="https://2026-05-07-tuner.antihq.com/" target="_blank">Tuner</flux:link></li>
         <li><flux:link href="https://2026-05-04-glance.antihq.com/" target="_blank">Glance</flux:link></li>
-        <li><flux:link href="{{ route('vault') }}">Vault</flux:link></li>
     </ul>
 
     <dl class="mt-6">
